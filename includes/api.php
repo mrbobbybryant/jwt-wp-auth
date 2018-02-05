eb9189fa56b2ff37690687214e5d9771de8bf763
Bobby Bryant <bobby@Bobbys-MacBook-Pro-2.local>
2018-02-05 17:48:38 -0500

    build endpoint used to request a password reset and email.

--- includes/api.php
+++ includes/api.php
@@ -36,6 +36,15 @@ function register_user_auth_route() {
 			'args'                => get_register_arguments(),
 		)
 	);
+
+	register_rest_route(
+		'jwt/v1', '/auth/reset_password', array(
+			'methods'             => \WP_REST_Server::EDITABLE,
+			'callback'            => __NAMESPACE__ . '\reset_password',
+			'permission_callback' => __NAMESPACE__ . '\reset_password_permissions_check',
+			'args'                => get_reset_arguments(),
+		)
+	);
 }
 
 /**
@@ -93,6 +102,186 @@ function register_user( $request ) {
 	$payload = apply_filters( 'jwt_wp_filter_register_response', $payload, $user );
 
 	return rest_ensure_response( $payload );
+}
+
+/**
+ * Callback function responsible for logging a user as requests a password reset link. Function
+ * Creates reset key, saves it to the DB, and sends the reset password link via an email.
+ *
+ * @param [Object] $request WP Rest API Request Object.
+ * @return Object|mixed Returns true on success, or an Error if request fails.
+ */
+function reset_password( $request ) {
+	global $wpdb, $wp_db_version;
+
+	if ( strpos( $request['user_login'], '@' ) ) {
+		$user_data = get_user_by( 'email', trim( $request['user_login'] ) );
+	} else {
+		$user_data = get_user_by( 'login', trim( $request['user_login'] ) );
+	}
+
+	$user_login = $user_data->user_login;
+	$user_email = $user_data->user_email;
+
+	// Generate something random for a password reset key.
+	$key = wp_generate_password( 20, false );
+
+	$hashed = wp_hash_password( $key );
+
+	$result = $wpdb->update( $wpdb->users, [ 'user_activation_key' => $hashed ], [ 'user_login' => $user_login ] );
+
+	if ( false === $result ) {
+		$error = new \WP_Error(
+			'jwt_reset_error',
+			'Fatal Error. Unable to request a password reset for this user. Please try again.',
+			array( 'status' => 401 )
+		);
+
+		return rest_ensure_response( $error );
+	}
+
+	$email = send_reset_password_email( $user_login, $key, $user_email, $user_data );
+
+	if ( is_wp_error( $email ) ) {
+		$error = new \WP_Error(
+			$email->get_error_code(),
+			$email->get_error_message(),
+			array( 'status' => 401 )
+		);
+
+		return rest_ensure_response( $error );
+	}
+
+	return true;
+}
+
+/**
+ * Function runs a number of checks to make sure that we have a valid reset password request.
+ *
+ * @param [Object] $request WP Rest API Request Object.
+ * @return Object|mixed Returns true on success, or an Error if request fails.
+ */
+function reset_password_permissions_check( $request ) {
+	if ( strpos( $request['user_login'], '@' ) ) {
+		$user_data = get_user_by( 'email', trim( $request['user_login'] ) );
+
+		if ( empty( $user_data ) ) {
+			$error = new \WP_Error(
+				'jwt_reset_error_email',
+				'A user with that email does not exist.',
+				array( 'status' => 401 )
+			);
+
+			return rest_ensure_response( $error );
+		}
+	} else {
+		$user_data = get_user_by( 'login', trim( $request['user_login'] ) );
+
+		if ( empty( $user_data ) ) {
+			$error = new \WP_Error(
+				'jwt_reset_error_username',
+				'A user with that username does not exist.',
+				array( 'status' => 401 )
+			);
+
+			return rest_ensure_response( $error );
+		}
+	}
+
+	$allow = apply_filters( 'allow_password_reset', true, $user_data->ID );
+
+	if ( ! $allow ) {
+		$error = new \WP_Error(
+			'jwt_reset_error',
+			'This website does not allow password resets.',
+			array( 'status' => 401 )
+		);
+
+		return rest_ensure_response( $error );
+	} elseif ( is_wp_error( $allow ) ) {
+		$error = new \WP_Error(
+			$allow->get_error_code(),
+			$allow->get_error_message(),
+			array( 'status' => 401 )
+		);
+
+		return rest_ensure_response( $error );
+	}
+
+	return true;
+}
+
+/**
+ * Function regestes the only valid argumnets that the reset password endpoint will accept.
+ *
+ * @return Array Associative array for the login schema.
+ */
+function get_reset_arguments() {
+	$args = array();
+
+	$args['user_login'] = array(
+		'description'       => esc_html__( 'A WordPress username.' ),
+		'type'              => 'string',
+		'required'          => true,
+		'validate_callback' => __NAMESPACE__ . '\string_arg_validate_callback',
+	);
+
+	return $args;
+}
+
+/**
+ * Function contains all the logic to actually send the reset password email.
+ *
+ * @param [string] $user_login WP User Name.
+ * @param [string] $key Generated Reset Password Key.
+ * @param [string] $user_email WP User Email.
+ * @param [object] $user_data WP User Object.
+ * @return Object|mixed Returns true on success, or an Error if request fails.
+ */
+function send_reset_password_email( $user_login, $key, $user_email, $user_data ) {
+	$url = add_query_arg(
+		array(
+			'rcp_action' => 'lostpassword_reset',
+			'key'        => $key,
+			'login'      => rawurlencode( $user_login )
+		),
+		JWT_ORIGIN
+	);
+
+	$url = apply_filters( 'retrieve_password_url', $url );
+
+	$message  = esc_html__( 'Someone requested that the password be reset for the following account:', 'jwt' ) . "\r\n\r\n";
+	$message .= network_home_url( '/' ) . "\r\n\r\n";
+	$message .= sprintf( esc_html__( 'Username: %s', 'jwt' ), $user_login ) . "\r\n\r\n";
+	$message .= esc_html__( 'If this was a mistake, just ignore this email and nothing will happen.', 'jwt' ) . "\r\n\r\n";
+	$message .= esc_html__( 'To reset your password, visit the following address:', 'jwt' ) . "\r\n\r\n";
+	$message .= esc_url_raw( $url ) . "\r\n";
+
+	if ( is_multisite() ) {
+
+		$blogname = $GLOBALS['current_site']->site_name;
+
+	} else {
+		/*
+		 * The blogname option is escaped with esc_html on the way into the database
+		 * in sanitize_option we want to reverse this for the plain text arena of emails.
+		 */
+		$blogname = wp_specialchars_decode( get_option( 'blogname' ), ENT_QUOTES );
+	}
+
+	$title   = sprintf( esc_html__( '[%s] Password Reset', 'jwt' ), $blogname );
+	$title   = apply_filters( 'retrieve_password_title', $title );
+	$message = apply_filters( 'retrieve_password_message', $message, $key, $user_login, $user_data );
+
+	if ( $message && ! wp_mail( $user_email, wp_specialchars_decode( $title ), $message ) ) {
+		return new \WP_Error(
+			'jwt_reset_error_email',
+			'The e-mail could not be sent, your host may have disabled the mail() function.',
+			array( 'status' => 401 )
+		);
+	}
+
+	return true;
 }
 
 /**
